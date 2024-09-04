1694916fbef6dd2f53de60e4ffa21c76f6c6adb9
linggash <sempanalingga@gmail.com>
2024-09-04 09:36:04 +0700

    Init project

---
+++ src/main/java/com/linggash/spring_webmvc_learning/SpringWebMvcLearningApplication.java
@@ -0,0 +1,13 @@
+package com.linggash.spring_webmvc_learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebMvcLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebMvcLearningApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/linggash/spring_webmvc_learning/SpringWebMvcLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linggash.spring_webmvc_learning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebMvcLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
