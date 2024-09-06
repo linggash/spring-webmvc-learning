c138a9a746e5b58b742bb44fb2381f1d541f4d01
linggash <sempanalingga@gmail.com>
2024-09-06 12:59:36 +0700

    Learned about PathVariable Annotation

---
+++ src/test/java/com/linggash/spring_webmvc_learning/controller/OrderControllerTest.java
@@ -0,0 +1,32 @@
+package com.linggash.spring_webmvc_learning.controller;
+
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.MockMvcBuilder.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class OrderControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void testOrderProduct() throws Exception {
+        mockMvc.perform(
+                get("/orders/2/products/3")
+        ).andExpectAll(
+                status().isOk(),
+                content().string(Matchers.containsString("Order : 2, Product : 3"))
+        );
+    }
+}
