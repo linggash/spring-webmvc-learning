6a11ee782834025984c3206499a2344f7640172c
linggash <sempanalingga@gmail.com>
2024-09-06 09:29:09 +0700

    Learned about Mock Bean

--- src/main/java/com/linggash/spring_webmvc_learning/controller/HelloController.java
+++ src/main/java/com/linggash/spring_webmvc_learning/controller/HelloController.java
@@ -1,23 +1,25 @@
 package com.linggash.spring_webmvc_learning.controller;
 
+import com.linggash.spring_webmvc_learning.service.HelloService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.io.IOException;
-import java.util.Objects;
 
 @Controller
 public class HelloController {
 
+    @Autowired
+    private HelloService helloService;
+
     @RequestMapping(path = "/hello")
     public void helloWorld(HttpServletRequest request,HttpServletResponse response) throws IOException {
         String name = request.getParameter("name");
-        if (Objects.isNull(name)) {
-            name = "Guest";
-        }
-        response.getWriter().println("Hello " + name);
+        String responseBody = helloService.hello(name);
+        response.getWriter().println(responseBody);
     }
 
 }
