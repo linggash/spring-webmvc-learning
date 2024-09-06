c2f8758dfbe795c6903711211759bd39561eff52
linggash <sempanalingga@gmail.com>
2024-09-06 09:53:55 +0700

    Learned about Request Param and Converter

--- src/main/java/com/linggash/spring_webmvc_learning/controller/HelloController.java
+++ src/main/java/com/linggash/spring_webmvc_learning/controller/HelloController.java
@@ -1,11 +1,11 @@
 package com.linggash.spring_webmvc_learning.controller;
 
 import com.linggash.spring_webmvc_learning.service.HelloService;
-import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.io.IOException;
 
@@ -16,8 +16,8 @@ public class HelloController {
     private HelloService helloService;
 
     @GetMapping(path = "/hello")
-    public void helloWorld(HttpServletRequest request,HttpServletResponse response) throws IOException {
-        String name = request.getParameter("name");
+    public void helloWorld(@RequestParam(name = "name", required = false) String name,
+                           HttpServletResponse response) throws IOException {
         String responseBody = helloService.hello(name);
         response.getWriter().println(responseBody);
     }
