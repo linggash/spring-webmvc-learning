894da29992de5e297498a2f3319b9e55d926bc29
linggash <sempanalingga@gmail.com>
2024-09-06 15:06:46 +0700

    Learned about @CookieValue

--- src/main/java/com/linggash/spring_webmvc_learning/controller/AuthController.java
+++ src/main/java/com/linggash/spring_webmvc_learning/controller/AuthController.java
@@ -1,9 +1,13 @@
 package com.linggash.spring_webmvc_learning.controller;
 
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -13,13 +17,22 @@ public class AuthController {
     @PostMapping(path = "/auth/login", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
     public ResponseEntity<String> login(
             @RequestParam(name = "username") String username,
-            @RequestParam(name = "password") String password
+            @RequestParam(name = "password") String password,
+            HttpServletResponse servletResponse
     ) {
         if ("bambang".equals(username) && "secret".equals(password)) {
+            Cookie cookie = new Cookie("username", username);
+            servletResponse.addCookie(cookie);
+
             return ResponseEntity.status(HttpStatus.OK).body("OK");
         } else {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("KO");
         }
+    }
+
+    @GetMapping(path = "/auth/user")
+    public ResponseEntity<String> getUser(@CookieValue("username") String username) {
+        return ResponseEntity.ok("Hello " + username);
     }
 
 }
