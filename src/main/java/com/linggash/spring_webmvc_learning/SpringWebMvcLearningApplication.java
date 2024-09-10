b8d0b7fe6eb2f2493f82eace5ee6f5eaf7ad667c
linggash <sempanalingga@gmail.com>
2024-09-10 09:28:52 +0700

    Learned about Error Page

--- src/main/java/com/linggash/spring_webmvc_learning/SpringWebMvcLearningApplication.java
+++ src/main/java/com/linggash/spring_webmvc_learning/SpringWebMvcLearningApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringWebMvcLearningApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)	 {
 		SpringApplication.run(SpringWebMvcLearningApplication.class, args);
 	}
 
