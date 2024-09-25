1f7f4b1afd7cd0ba98119fbb59e3844974c3a3b3
linggash <sempanalingga@gmail.com>
2024-09-25 08:20:46 +0700

    Learned about Servlet Integration

--- src/main/java/com/linggash/spring_webmvc_learning/SpringWebMvcLearningApplication.java
+++ src/main/java/com/linggash/spring_webmvc_learning/SpringWebMvcLearningApplication.java
@@ -3,12 +3,14 @@ package com.linggash.spring_webmvc_learning;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 import java.time.Duration;
 
 @SpringBootApplication
+@ServletComponentScan
 public class SpringWebMvcLearningApplication {
 
 	@Bean
